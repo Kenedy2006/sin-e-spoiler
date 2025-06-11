bbee688ea40576c21d7fe4c9c33576e79090b1cf
Kenedy2006 <jeyson.ramos@tecsup.edu.pe>
2025-06-11 17:03:23 -0500

    Sin-e-spoiler

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <>
+    <Header />
+    <h2>Las mejores películas de la historia</h2>
+    <Footer />
+  )
+}
+
+export default App
